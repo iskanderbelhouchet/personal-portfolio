18d372d02077624aa6b1b1e04511562ed1f316fe
iskanderbelhouchet <78920370+iskanderbelhouchet@users.noreply.github.com>
2022-07-10 20:19:56 +0100

    Personal portfolio

---
+++ Frontend/src/components/Newsletter.js
@@ -0,0 +1,30 @@
+import { useState, useEffect } from 'react'
+import { Col, Row, Alert } from 'react-bootstrap'
+
+export const Newsletter = ({ status, message, onValidated }) => {
+  const [email, setEmail] = useState('')
+
+  useEffect(() => {
+    if (status === 'success') clearFields()
+  }, [status])
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    email &&
+    email.indexOf('@') > -1 &&
+    onValidated({
+      EMAIL: email
+    })
+  }
+
+  const clearFields = () => {
+    setEmail('')
+  }
+
+  return (
+    <Col lg={12}>
+      <div className='newsletter-bx wow slideInUp'>
+      </div>
+    </Col>
+  )
+}
